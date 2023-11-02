7123f275ce28ac91d3d4e69e0548fd2b175c02a1
sushant <118897403+sushant-demn@users.noreply.github.com>
2023-11-02 18:52:55 +0000

    she said do u love i said only partly i only love my bed and my mom im sorry

---
+++ college.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+class person
+{
+private:
+    class student
+    {
+    };
+    class professor
+    {
+    };
+};
+int main()
+{
+
+    return 0;
+}
--- test2.cpp
+++ test2.cpp
@@ -1,7 +1,33 @@
-# include<iostream>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
-int main(){
-    
+
+int main() {
+      int a;
+    cin >> a;
+    string usrnm[1000];
+    for (int i = 0; i < a; i++)
+    {
+        cin >> usrnm[i];
+    };
+    for (int i = 0; i < a; i++)
+    {   int b = usrnm[i].length();
+        if (b < 5)
+        {
+            cout<<"Too short: "<<usrnm[i].length()<<"\n";
+        }
+        else{
+            if(i % 2 = 0){
+                cout<<"valid\n";
+            }
+            else{
+
+            }
+        }
+    }   
     return 0;
 }
