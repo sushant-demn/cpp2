27d465ed719283b276103d367882bef650c1313c
sushant <118897403+sushant-demn@users.noreply.github.com>
2023-11-20 14:39:40 +0000

    ok sup

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,24 +1,16 @@
 #include <iostream>
 using namespace std;
-void chkrow(int a[9][9])
-{
-    for (int i = 0; i < 9; i++)
-    {
-        for (int j = 0; j < 9; j++)
-        {
+void chk(void){
+    int a[18];
 
-            if (a[i][0] = j)
-            {
-            }
-        }
+        
     }
-};
 int main()
 {
     int num[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     int arr[9][9];
 
-    for (size_t row = 0; row < 9; row++)
+    for (size_t row = 0; row < 9; row++) // TaKing a sudoku as a 9x9 matrix where 0 denotes empty spaces
     {
         for (size_t col = 0; col < 9; col++)
         {
@@ -26,7 +18,21 @@ int main()
         };
     };
     cout << "\n\n\n";
-   
+
+for (int i = 0; i < 3; i++) //check for empty spaces in the sudoku
+{
+    for (int j = 0; j < 3; j++)
+    {
+        if (arr[i][j] == 0)
+        {
+            
+        }
+        
+    }
+    
+}
+
+    
 
     return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,7 +1,1 @@
- // for (size_t row = 0; row < 9; row++)
-    // {
-    //     for (size_t col = 0; col < 9; col++)
-    //     {
-    //         cout << arr[row][col] << (col + 1 < 9 ? " " : "\n");
-    //     }
-    // }
+1
