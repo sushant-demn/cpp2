d944be20316382f71f3b7b0b48b638491d261ae7
sushant <118897403+sushant-demn@users.noreply.github.com>
2024-06-05 14:32:56 +0000

    takes a 3*3 array as input in magic_sqr

---
+++ questions/magic_sqr.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[3][3] = {0};
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    return 0;
+}  
