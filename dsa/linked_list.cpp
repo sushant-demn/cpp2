a37352e9b79cc1337e9a641c105ad57a5a2bbac3
sushant-demn <sushantpayal2200@gmail.com>
2024-08-03 16:53:28 +0530

    linked_list

--- dsa/linked_list.cpp
+++ dsa/linked_list.cpp
@@ -18,9 +18,58 @@ void insertion(int new_data) // insertion at beginning
 
 void insertion_end(int new_data)
 {
-    struct node *ptr = (struct node *)malloc(sizeof(struct node));
-    ptr->data = new_data;
-    ptr->next = NULL;
+    struct node *new_node = (struct node *)malloc(sizeof(struct node));
+    new_node->data = new_data;
+    if (head == NULL)
+    {
+
+        new_node->next = head;
+        head = new_node;
+    }
+    else
+    {
+
+        struct node *ptr;
+        ptr = head;
+        while (ptr->next != NULL)
+        {
+            ptr = ptr->next;
+        }
+        ptr->next = new_node;
+        new_node->next = NULL;
+    }
+}
+
+void insertion_pos(int new_data, int pos)
+{
+    struct node *new_node = (struct node *)malloc(sizeof(struct node));
+    new_node->data = new_data;
+    int count = 0;
+    if (pos == 1)
+    {
+        new_node->next = head;
+        head = new_node;
+    }
+    else
+    {
+        struct node *prev;
+        struct node *ptr;
+        while (ptr != NULL)
+        {
+            count++;
+            if (count == pos)
+            {
+                break;
+            }
+            else
+            {
+                prev = ptr;
+                ptr = ptr->next;
+            }
+            new_node->next = ptr;
+            prev->next = new_node;
+        }
+    }
 }
 
 void display()
@@ -35,9 +84,12 @@ void display()
 
 int main()
 {
-    insertion(2);
-    insertion(3);
-    insertion(4);
+    insertion_end(5);
+    insertion_end(6);
+    insertion_end(7);
+    insertion_pos(8, 1);
+    insertion_pos(9, 3);
+
     display();
     return 0;
 }
